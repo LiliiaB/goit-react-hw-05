ec7381b87c7c8972231be7ce101ffed826f94777
Lilia Levchenko <liliya.benatti@gmail.com>
2024-04-07 13:25:59 +0200

    structure update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,14 +4,24 @@ import { MovieCast } from "../MovieCast/MovieCast";
 import { MovieList } from "../MovieList/MovieList";
 import { MovieReview } from "../MovieReview/MovieReview";
 import { Navigation } from "../Navigation/Navigation";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+const HomePage = lazy(() => import("../pages/HomePage"));
 
 export default function App() {
   return (
     <>
-      <MovieCast />
-      <MovieReview />
-      <MovieList />
-      <Navigation />
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">Home</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <MovieCast />
+        <MovieReview />
+        <MovieList />
+        <Navigation />
+      </Routes>
     </>
   );
 }
