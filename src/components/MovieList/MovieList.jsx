05a606628dab28215af1ae753f01e0d786fb6536
Lilia Levchenko <liliya.benatti@gmail.com>
2024-04-12 22:14:28 +0200

    movies upd

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import css from "./MovieList.module.css";
 
 export const MovieList = ({ movies }) => {
+  const location = useLocation();
   return (
     <div>
       <ul className={css.list}>
         {movies.map((movie) => {
           return (
             <li key={movie.id}>
-              {movie.title} <Link to={`/movies/${movie.id}`}></Link>
+              {movie.title}
+              <Link
+                to={`/movies/${movie.id}`}
+                state={{ from: location }}
+              ></Link>
             </li>
           );
         })}
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,9 +1,55 @@
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 import SearchBar from "../components/SearchBar/SearchBar";
+import MovieList from "../components/MovieList/MovieList";
+import { searchMovie } from "../components/API/api";
+import { useEffect, useState } from "react";
 
 export const MoviesPage = () => {
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [moviesList, setMoviesList] = useState([]);
+
+  const movieName = searchParams.get("movieName") ?? "";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await searchMovie(movieName);
+        setMoviesList(data.results);
+      } catch (error) {
+        console.error("Error fetching movie data:", error);
+      }
+    };
+
+    if (movieName !== "") {
+      fetchData();
+    } else {
+      setMoviesList([]);
+    }
+  }, [movieName]);
+
+  const handleSubmit = (event) => {
+    console.log(handleSubmit);
+    event.preventDefault();
+    const searchForm = event.currentTarget;
+    setSearchParams({ movieName: searchForm.elements.movieName.value });
+    searchForm.reset();
+  };
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSubmit={handleSubmit} />
+      <MovieList movies={moviesList}>
+        {moviesList.map((movie) => {
+          return (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+                {movie.title}
+              </Link>
+            </li>
+          );
+        })}
+      </MovieList>
     </div>
   );
 };
